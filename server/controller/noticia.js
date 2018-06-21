79adc66ff0282c6a8821c7d3d2b94156289b6847
julio <juliocesardm93@gmail.com>
2018-06-21 13:30:35 -0400

    Added: se agrega metodos del controlador noticias 2

--- server/controller/noticia.js
+++ server/controller/noticia.js
@@ -1,6 +1,7 @@
 /**
- * Created By Eduardo on 10/05/2018
- */
+<<<<<<< HEAD
+ *  Created by Julio, Eduardo, Joyce, Franz on 10/05/2018
+ * */
 'use strict';
 
 const Noticia = require('../model/noticia');
@@ -17,7 +18,7 @@ function crearNoticia(req,res) {
     noticia.categoria = params.categoria;
     noticia.tag = params.tag;
 
-    noticia.save((err, noticia_guardada) => {
+    Noticia.save((err, noticia_guardada) => {
         if(err){
             res.status(500).send({
                 desc: 'Error en el servidor',
@@ -26,16 +27,90 @@ function crearNoticia(req,res) {
         }else {
             if(!noticia_guardada) {
                 res.status(404).send({
-                    desc:'Noticia no guardada'
+                    desc:'Noticia no guardada',
                 })
             }else {
                 res.status(200).send(noticia_guardada);
             }
+        }
+    })
+}
 
+function getNoticia(req,res){
+
+    const idNoticia=req.params.id;
+
+    Noticia.findOne({_id:idNoticia},(err,noticia_encontrada)=>{
+
+        if(err){
+            res.status(500).send({
+                desc: 'Error en el servidor',
+                err: err.message
+            })
+        }else{
+            if(!noticia_encontrada){
+                res.status(404).send({
+                    desc: 'Noticia no encontrada',
+                    err: err.message
+                })
+            }else{
+                res.status(200).send(noticia_encontrada);
+            }
+        }
+    })
+}
+
+function modificarNoticia(req,res){
+
+    const idNoticia=req.params.id;
+
+    Noticia.findOne({_id:idNoticia},(err,noticia_modificada)=>{
+
+        if(err){
+            res.status(500).send({
+                desc: 'Error en el servidor',
+                err: err.message
+            })
+        }else{
+            if(!noticia_modificada){
+                res.status(404).send({
+                    desc: 'Noticia no encontrada',
+                    err: err.message
+                })
+            }else{
+
+                noticia_modificada.titulo = params.titulo;
+                noticia_modificada.fecha = params.fecha;
+                noticia_modificada.cuerpo = params.cuerpo;
+                noticia_modificada.autor = params.autor;
+                noticia_modificada.categoria = params.categoria;
+                noticia_modificada.tag = params.tag;
+
+                noticia_modificada.save((err, noticia_modificada) => {
+                    if(err){
+                        res.status(500).send({
+                            desc: 'Error en el servidor',
+                            err: err.message
+                        })
+                    }else {
+                        if(!noticia_modificada) {
+                            res.status(404).send({
+                                desc:'Noticia no guardada'
+                            })
+                        }else {
+                            res.status(200).send(noticia_modificada);
+                        }
+                    }
+                })
+            }
         }
     })
 }
 
 module.exports = {
-    crearNoticia
+    crearNoticia,
+    getNoticia,
+    modificarNoticia,
+
 };
+
--- server/routes/noticia.js
+++ server/routes/noticia.js
@@ -7,5 +7,9 @@ const NoticiaController = require('../controller/noticia');
 const midleware = require('../midleware/midleware');
 
 const api = express.Router();
+
 api.post('/crearNoticia',midleware.ensureAuth, NoticiaController.crearNoticia);
+api.post('/getNoticia',midleware.ensureAuth, NoticiaController.getNoticia);
+api.post('/modificarNoticia',midleware.ensureAuth, NoticiaController.modificarNoticia);
+
 module.exports = api;
